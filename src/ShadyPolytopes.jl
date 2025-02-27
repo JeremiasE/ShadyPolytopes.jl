792a15a029e85cdf7ef07a732d6cdef55c6a713e
Jeremias Epperlein <jeremias.epperlein@gmail.com>
2025-02-27 16:32:36 +0100

    Add exports for sos_models

--- src/ShadyPolytopes.jl
+++ src/ShadyPolytopes.jl
@@ -5,6 +5,7 @@ include("rounding.jl")
 include("analyse_ball.jl")
 include("perturbed_icosahedron.jl")
 include("farkas.jl")
+include("sos_models.jl")
 
 export fast_round
 export CSCB, calculate_normals, all_vertices
@@ -13,5 +14,8 @@ export determine_matrix_ball_vertices, determine_squared_spectralnorm_bound
 export approximate_john_position, approximate_projection, operator_norm
 export find_single_farkas_certificate, matrix_from_projection_normal
 export generate_farkas_certificates, check_single_farkas_certificate, check_farkas_cerfificate_file
+export putinar_model, solve_via_projection_matrix, solve_via_vw
+export shadiness_via_projection_matrix
+export shadiness_via_vw
 
 end
