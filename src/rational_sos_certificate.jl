858c5631d9ff1be0094589a2840e49fe34c2ce1f
Jeremias Epperlein <jeremias.epperlein@gmail.com>
2025-03-14 09:49:06 +0100

    Moving offset calculation yet again

--- src/rational_sos_certificate.jl
+++ src/rational_sos_certificate.jl
@@ -212,10 +212,11 @@ function round_sos_decomposition(model, K, obj, vars, squared_variable_bound,
 
     println("[Calculating offset weights.]")
     #return new_monos,soss,ineqs,offset,vars
+    
+    Δ, offset_sos = offset_sum_of_monomials(new_monos[2:end],vars; n=squared_variable_bound)
     if isnothing(offset)
         offset = big(1)//(2Δ)
     end
-    Δ, offset_sos = offset_sum_of_monomials(new_monos[2:end],vars; n=squared_variable_bound)
     for i in eachindex(vars)
         offset_sos[i].wv *= offset
     end
